769ba9614dd123fb72e3053bc54aa937bc76568b
Israel <almanzaisrael75@gmail.com>
2024-01-21 15:29:05 -0400

    administrador multi-dipositivos

--- src/core/api/ConfigSwisse.tsx
+++ src/core/api/ConfigSwisse.tsx
@@ -1,12 +1,6 @@
 const ConfigSwisse = {
-
-  
    urlapi: 'http://localhost:3333',
-   //urlapi: 'http://192.168.0.18:8080',
    urlApiWhatsapp:"http://localhost:3333",
-
-   // urlapi: 'https://sistemageneralb.azurewebsites.net/', //lo correcto
-   // urlapi: 'https://sistemageneralb.azurewebsites.net/',
 }
 
 export default ConfigSwisse;
